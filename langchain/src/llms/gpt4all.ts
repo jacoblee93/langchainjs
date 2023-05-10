c90c3e3d1d4fce62fa886abb81c5b056782f603d
Jacob Lee <jacoblee93@gmail.com>
2023-05-10 09:42:39 -0700

    Adds a GPT4All LLM class

--- langchain/src/llms/gpt4all.ts
+++ langchain/src/llms/gpt4all.ts
@@ -1,18 +1,27 @@
+import { GPT4All as GPT4AllClient } from "gpt4all";
+
 import { LLM, BaseLLMParams } from "./base.js";
 
 export interface GPT4AllInput {
-  // These are the only two models supported by the gpt4all-ts library currently
+  // These are the only two models supported by the gpt4all-ts library currently.
+  // TODO: The gpt4all TS package does not currently export an input type - if they do, use it here.
   model: "gpt4all-lora-unfiltered-quantized" | "gpt4all-lora-quantized";
   forceDownload?: boolean;
-  decoderConfig?: Record<string, unknown>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  decoderConfig?: Record<string, any>;
 }
 
-export class GPT4All extends LLM implements GPT4AllInput {
-  model: GPT4AllInput["model"];
+export class GPT4All extends LLM {
+  private model: GPT4AllInput["model"];
 
-  forceDownload: boolean;
+  private forceDownload: boolean;
 
-  decoderConfig: Record<string, unknown>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private decoderConfig: Record<string, any>;
+
+  private gpt4AllClient: GPT4AllClient;
+
+  private isInitialized = false;
 
   constructor(fields: GPT4AllInput & BaseLLMParams) {
     super(fields);
@@ -20,43 +29,35 @@ export class GPT4All extends LLM implements GPT4AllInput {
     this.model = fields.model;
     this.forceDownload = fields.forceDownload ?? false;
     this.decoderConfig = fields.decoderConfig ?? {};
+    this.gpt4AllClient = new GPT4AllClient(
+      this.model,
+      this.forceDownload,
+      this.decoderConfig
+    );
   }
 
   _llmType() {
     return "gpt4all";
   }
 
-  /** @ignore */
-  async _call(prompt: string, _stop?: string[]): Promise<string> {
-    const imports = await GPT4All.imports();
-
-    const gpt4all = new imports.GPT4All(
-      this.model,
-      this.forceDownload,
-      this.decoderConfig
-    );
-    await gpt4all.init();
-    await gpt4all.open();
-
-    const output = await this.caller.call(
-      async () => await gpt4all.prompt(prompt)
-    );
-
-    gpt4all.close();
-    return output;
+  async initialize() {
+    await this.gpt4AllClient.init();
+    this.isInitialized = true;
   }
 
-  /** @ignore */
-  static async imports(): Promise<{
-    GPT4All: typeof import("gpt4all").GPT4All;
-  }> {
-    try {
-      const { GPT4All } = await import("gpt4all");
-      return { GPT4All };
-    } catch (e) {
-      throw new Error(
-        "Please install gpt4all as a dependency with, e.g. `yarn add gpt4all`"
-      );
+  async _call(prompt: string, _stop?: string[]): Promise<string> {
+    if (!this.isInitialized) {
+      await this.gpt4AllClient.init();
+      this.isInitialized = true;
     }
+
+    await this.gpt4AllClient.open();
+
+    const output = await this.caller.call(
+      this.gpt4AllClient.prompt.bind(null, prompt)
+    );
+
+    this.gpt4AllClient.close();
+    return output;
   }
 }
--- langchain/src/llms/tests/gpt4all.int.test.ts
+++ langchain/src/llms/tests/gpt4all.int.test.ts
@@ -9,13 +9,36 @@ test(
     const model = new GPT4All({
       model: "gpt4all-lora-quantized",
     });
+
+    const res = await model.call("Hello, my name is ");
+
     const endTime = performance.now();
     const timeElapsed = endTime - startTime;
     console.log(`GPT4All: Time elapsed: ${timeElapsed} milliseconds`);
 
-    const res = await model.call("Hello, my name is ");
-
     expect(typeof res).toBe("string");
+  },
+  600 * 1000
+);
+
+test(
+  "Test GPT4All with multiple concurrent uninitialized calls",
+  async () => {
+    const startTime = performance.now();
+    const model = new GPT4All({
+      model: "gpt4all-lora-quantized",
+    });
+
+    const responses = await Promise.all([
+      model.call("Hello, my name is "),
+      model.call("What color is the sky?"),
+      model.call("Who are you?"),
+    ]);
+    const endTime = performance.now();
+    const timeElapsed = endTime - startTime;
+    console.log(`GPT4All: Time elapsed: ${timeElapsed} milliseconds`);
+
+    console.log({ responses });
   },
   600 * 1000
 );
